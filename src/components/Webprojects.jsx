0274c42b90991f52b6d6a50bab910e27f81e739b
Wapitifou <marianne.bonnard.nz@gmail.com>
2025-01-31 06:39:58 +0100

    MAJ contenu page Webprojects

--- src/components/Webprojects.jsx
+++ src/components/Webprojects.jsx
@@ -3,53 +3,57 @@ import React from "react";
 const WebProjects = () => {
   return (
     <div className="showcase">
-      <div className="project-card">
-        <div className="recom-culture">
+      <div className="recom-culture">
+        <div className="intro">
           <h2>Recom & Culture</h2>
           <a href="">
             <img src="Recom-culture.png" alt="recom-&-culture screenshot" />
           </a>
-          <div className="info">
-            <h3>
-              A website to share your cultural recommandations with your friends
-            </h3>
-            <p>
-              The idea here is to share with friends a common database of what
-              each of you has watched, read, visited and their thoughts on it.
-              The "Keep in mind" function allows the user to keep track of what
-              they want to watch/ read/ see later.
-            </p>
-          </div>
         </div>
-        <div className="nature-western">
+        <div className="info">
+          <h3>
+            A website to share your cultural recommandations with your friends
+          </h3>
+          <p>
+            The idea here is to share with friends a common database of what
+            each of you has watched, read, visited and their thoughts on it. The
+            "Keep in mind" function allows the user to keep track of what they
+            want to watch/ read/ see later.
+          </p>
+        </div>
+      </div>
+      <div className="nature-western">
+        <div className="intro">
           <h2>Ecuries Nature Western</h2>
           <a href="">
             <img src="" alt="pic-to-come" />
           </a>
-          <div className="info">
-            <h3>A showcase website for a western horse riding ranch</h3>
-            <p>
-              The owner needed a new website as she developed her activity. With
-              her permission I created this website to show her some ideas of
-              what it could be.
-            </p>
-          </div>
         </div>
-        <div className="quizz">
+        <div className="info">
+          <h3>A showcase website for a western horse riding ranch</h3>
+          <p>
+            The owner needed a new website as she developed her activity. With
+            her permission I created this website to show her some ideas of what
+            it could be.
+          </p>
+        </div>
+      </div>
+      <div className="quizz">
+        <div className="intro">
           <h2>Unnecessary info quizz</h2>
           <a href="">
             <img src="Useless-Quizz.png" alt="Useless-Quizz screenshot" />
           </a>
-          <div className="info">
-            <h3>
-              A quizz dedicated to learning useless informations about the world
-            </h3>
-            <p>
-              Working on this project was interesting to pull out logical
-              frameworks and bring the output to users in a aesthetic design
-              with dynamic content.
-            </p>
-          </div>
+        </div>
+        <div className="info">
+          <h3>
+            A quizz dedicated to learning useless informations about the world
+          </h3>
+          <p>
+            Working on this project was interesting to pull out logical
+            frameworks and bring the output to users in a aesthetic design with
+            dynamic content.
+          </p>
         </div>
       </div>
     </div>
